2f251907ddd7e8454a4f55a6a6ee5618be0b677b
Jesse Väärälä <jesse.vaarala@gmail.com>
2020-09-10 20:40:26 +0300

    example

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Tyontekijat :tableItems="tyontekijatArray"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Tyontekijat from "@/components/Tyontekijat";
+    import json from './assets/tyontekijat.json'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        components: {
+            Tyontekijat,
+        },
+        data: function () {
+            return {
+                tyontekijatArray: json.tyontekijat
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/Tyontekijat.vue
@@ -0,0 +1,18 @@
+<template>
+    <div style="width: 75%; margin: 0 auto">
+        <b-table striped hover :items="tableItems"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Tyontekijat",
+        props: {
+            tableItems: Array
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
